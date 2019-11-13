6003ff04f7c4574d4db889009c2874fa15ad7b61
zsr <578384741@qq.com>
2019-11-13 04:55:50 -0800

    homework

--- week10/homework/fork_test.c
+++ week10/homework/fork_test.c
@@ -3,6 +3,8 @@
 int main(){
 	FILE *fp;
 	int i;
+	char buf1[] = {"abcdef"};
+    char buf2[] = {"123456"};
 	pid_t pid;
 	if((fp = fopen("fork_test.dat","a+"))==NULL)
 	{
@@ -12,14 +14,18 @@ int main(){
 	pid = fork();
 	if(pid<0)
 	{
-		perror("vfork dailed!\n");
+		perror("fork dailed!\n");
 		exit(-1);	
-	}else if(pid == 1){  //子进程
-		for(i=0;i<9;i++)
-			fprintf(fp,"%d",i);	
+	}else if(pid == 0){  //子进程
+		//for(i=0;i<9;i++)
+		//	fprintf(fp,"%d",i);	
+		fputs(buf1,fp);
+		exit(0);
 	}else{               //父进程
-		for(i=0;i<9;i++)
-			fprintf(fp,"%d",i);
+		//for(i=0;i<9;i++)
+		//	fprintf(fp,"%d",i);	
+		fputs(buf2,fp);
+		exit(0);
 	}
 	return 0;
 }
--- week10/homework/vfork_test.c
+++ week10/homework/vfork_test.c
@@ -4,6 +4,8 @@ int main(){
 	FILE *fp;
 	int i;
 	pid_t pid;
+	char buf1[] = {"abcdef"};
+	char buf2[] = {"123456"};
 	if((fp = fopen("vfork_test.dat","a+"))==NULL)
 	{
 		perror("failed to create file!\n");
@@ -14,12 +16,16 @@ int main(){
 	{
 		perror("vfork dailed!\n");
 		exit(-1);	
-	}else if(pid == 1){  //子进程
-		for(i=0;i<9;i++)
-			fprintf(fp,"%d",i);	
+	}else if(pid == 0){  //子进程
+		//for(i=0;i<9;i++)
+		//	fprintf(fp,"%d",i);	
+		fputs(buf1,fp);
+		exit(0);
 	}else{               //父进程
-		for(i=0;i<9;i++)
-			fprintf(fp,"%d",i);
+		//for(i=0;i<9;i++)
+		//	fprintf(fp,"%d",i);	
+		fputs(buf2,fp);
+		exit(0);
 	}
 	return 0;
 }
